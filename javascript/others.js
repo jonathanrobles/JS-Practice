72aa96b45ef57fd0d9a9aa119eebb015ad89e10b
Jonathan Robles <111263727+jonathanrobles@users.noreply.github.com>
2023-08-19 22:07:28 +0800

    Added new js file for other js concept notes

---
+++ javascript/others.js
@@ -0,0 +1,25 @@
+// JS Output 
+    // innerHTML - to print in HTML format
+    // document.write() - for testing purposes
+    // window.alert - alert box
+    // console.log - debugginh purposes
+    // window.print - to print 
+
+// Two types of values
+    //Fixed Values - Literals
+    // Variable Values -Variables
+
+ var x = 10;
+    x + 1; // This is an expression 
+    // The output is called evaluation
+
+    let y = 10;
+    y = 9;
+
+    document.getElementById("num").innerHTML = y;
+
+    // var - can be redeclared
+    // let - cannot be redeclared (block scope)
+    // const - used for Objects, Arrays, Functions, and Regular Expressions
+
+    // Operator Precedence - PEMDAS
